878e0e59d2cf84113141faeaaa84d0f6d2331d36
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2022-01-19 05:11:08 +0700

    just reuse the extractor

---
+++ src/extractor.c
@@ -0,0 +1,211 @@
+#include "extractor.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s input_file output_file\n", argv[0]);
+        return 1;
+    }
+
+    FILE *area_file = fopen(argv[1], "rb");
+    if (area_file == NULL)
+    {
+        printf("Error opening input file\n");
+        return 2;
+    }
+
+    byte toc_header[16];
+    if (fread(toc_header, 1, sizeof(toc_header), area_file) != sizeof(toc_header))
+    {
+        printf("Error reading file!\n");
+        fclose(area_file);
+        return 3;
+    }
+    
+    if (!is_math_tbl(toc_header))
+    {
+        printf("Not an .EMI file!\n");
+        fclose(area_file);
+        return 4;
+    }
+
+    word file_count = convert_little_endian(toc_header, 0, 4);
+    word address;
+    word section_size;
+
+    if ((address = find_dialogue_section(area_file, file_count, &section_size)) == 0)
+    {
+        printf("Dialogue section not found!\n");
+        fclose(area_file);
+        return 5;
+    }
+    fseek(area_file, address, SEEK_SET);
+
+    FILE *output_file = fopen(argv[2], "w");
+    if (output_file == NULL)
+    {
+        printf("Error opening output file\n");
+        fclose(area_file);
+        return 6;
+    }    
+
+    byte dialogue_section[section_size];
+    if (fread(dialogue_section, 1, sizeof(dialogue_section), area_file) != sizeof(dialogue_section))
+    {
+        printf("Error reading file!\n");
+        fclose(area_file);
+        fclose(output_file);
+        return 7;
+    }
+
+    word pointer_size = convert_little_endian(dialogue_section, 0, 2);
+    word pointer;
+
+    char punct;
+    char last_color[8];
+    char symbol[4];
+    char box_position[20];
+    int j;
+    int opt_counter = 0;
+    int in_opt = 0;
+
+    for (int i = 0; i < pointer_size; i++)
+    {
+        pointer = convert_little_endian(dialogue_section, i * 2, 2);
+        j = pointer;
+        if (pointer == pointer_size && i != 0)
+        {
+            continue;
+        }
+        else if (pointer == section_size)
+        {
+            break;
+        }
+
+        while ((dialogue_section[j] != 0x00 || opt_counter > 0) && dialogue_section[j] != 0x16)
+        {
+            if (is_alphanum(dialogue_section[j]))
+            {
+                fprintf(output_file, "%c", dialogue_section[j]);
+            }        
+            else if (dialogue_section[j] == 0x0c)
+            {
+                strcpy(box_position, is_position(dialogue_section[j + 1]));
+                fprintf(output_file, "%s", box_position);
+                j++;
+            }
+            else if (dialogue_section[j] == 0xff)
+            {
+                fprintf(output_file, " ");
+            }
+            else if (dialogue_section[j] == 0x0d)
+            {
+                fprintf(output_file, "[EFFECT]");
+            }
+            else if (dialogue_section[j] == 0x01)
+            {
+                fprintf(output_file, "\n");
+            }
+            else if (dialogue_section[j] == 0x0b)
+            {
+                fprintf(output_file, "<>");
+            }
+            else if (dialogue_section[j] == 0x02)
+            {
+                fprintf(output_file, "\n|\n");
+            }
+            else if (dialogue_section[j] == 0x0f && dialogue_section[j - 1] == 0x0e)
+            {
+                fprintf(output_file, "[/EFFECT %s]", is_effect(dialogue_section[j + 1]));
+                j += 2;    
+            }
+            else if (dialogue_section[j] == 0x04)
+            {
+                switch (dialogue_section[j + 1])
+                {
+                    case 0x00:
+                        fprintf(output_file, "Ryu");
+                        j++;                    
+                        break;
+                    case 0x01:
+                        fprintf(output_file, "Nina");
+                        j++;                    
+                        break;
+                    case 0x02:
+                        fprintf(output_file, "Garr");
+                        j++;                    
+                        break;
+                    case 0x03:
+                        fprintf(output_file, "Teepo");
+                        j++;                    
+                        break;
+                    case 0x04:
+                        fprintf(output_file, "Rei");
+                        j++;                    
+                        break;
+                    case 0x05:
+                        fprintf(output_file, "Momo");
+                        j++;                    
+                        break;
+                    case 0x06:
+                        fprintf(output_file, "Peco");
+                        j++;                    
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (dialogue_section[j] == 0x05)
+            {
+                strcpy(last_color, is_color(dialogue_section[j + 1]));
+                fprintf(output_file, "[%s]", last_color);
+                j++;            
+            }
+            else if (dialogue_section[j] == 0x06)
+            {
+                fprintf(output_file, "[/%s]", last_color);
+            }
+            else if (dialogue_section[j] == 0x14)
+            {
+                fprintf(output_file, "\n[OPTIONS]\n");
+                opt_counter = dialogue_section[j + 3] & 0xf;
+                in_opt = 1;
+                j += 3;
+            }
+            else if (strcmp(strcpy(symbol, is_symbol(dialogue_section[j])), "") != 0)
+            {
+                fprintf(output_file, "%s", symbol);
+            }
+            else if ((punct = is_punct(dialogue_section[j])) != 0x00)
+            {
+                fprintf(output_file, "%c", punct);
+            }
+            else if (dialogue_section[j] == 0x07)
+            {
+                fprintf(output_file, "[PLACEHOLDER]");
+                j++;
+            }
+            else if (dialogue_section[j] == 0x00 && opt_counter > 0)
+            {
+                fprintf(output_file, "\n");
+                opt_counter--;
+            }
+
+            if (in_opt == 1 && opt_counter == 0)
+            {
+                fprintf(output_file, "[/OPTION]");
+                in_opt = 0;
+                break;
+            }
+
+            j++;
+        }
+
+        fprintf(output_file, "\n==================================================== [%i (0x%04x)]\n", i, pointer);
+    }
+
+    fclose(area_file);
+    fclose(output_file);
+    return 0;
+}
---
+++ src/utils.c
@@ -0,0 +1,324 @@
+#include "extractor.h"
+
+word convert_little_endian(byte a[], int n, int bytes)
+{
+    word result = 0;
+    for (int i = bytes - 1; i >= 0; i--)
+    {
+        result = a[n + i] << 8 * i | result;
+    }
+    return result;
+}
+
+word find_dialogue_section(FILE *f, word count, word *section_size)
+{
+    byte toc_entry[16];
+    word tmp = 0;
+    word address = 0x0800;
+    do
+    {
+        address += tmp;
+        if (fread(toc_entry, 1, sizeof(toc_entry), f) != sizeof(toc_entry))
+        {
+            return -1;
+        }
+
+        if (is_dialogue_section(toc_entry))
+        {
+            *section_size = convert_little_endian(toc_entry, 0, 4);
+            return address;
+        }
+
+        tmp = convert_little_endian(toc_entry, 0, 4);
+        if (tmp % 0x0800 != 0)
+        {
+            tmp = 0x0800 * ((tmp + 0x0800) / 0x0800);
+        }
+    } 
+    while (count--);
+    return 0;
+}
+
+bool is_math_tbl(byte toc_header[])
+{
+    byte magic[8] = {0x4d, 0x41, 0x54, 0x48, 0x5f, 0x54, 0x42, 0x4c};
+    for (int i = 8; i < 16; i++)
+    {
+        if (toc_header[i] != magic[i - 8])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool is_dialogue_section(byte toc_entry[])
+{
+    return toc_entry[4] == 0x00 && toc_entry[5] == 0x00 && toc_entry[6] == 0x01 && toc_entry[7] == 0x80;
+}
+
+bool is_alphanum(byte a)
+{
+    return (a >= 65 && a <= 90) || (a >= 97 && a <= 122) || (a >= 48 && a <= 57);
+}
+
+char is_punct(byte a)
+{
+    switch (a)
+    {
+        case 0x5d:
+            return '!';
+            break;
+        case 0x90:
+            return '"';
+            break;
+        case 0x8e:
+            return '\'';
+            break;
+        case 0x3c:
+            return ',';
+            break;
+        case 0x3d:
+            return '-';
+            break;
+        case 0x3e:
+            return '.';
+            break;
+        case 0x5c:
+            return '?';
+            break;
+        case 0x8f:
+            return ':';
+            break;
+        case 0x91:
+            return ';';
+            break;
+        default:
+            return 0x00;
+            break;
+    }
+}
+
+char *is_position(byte a)
+{
+    static char buffer[20];
+    byte position = a & 0x0f;
+    byte style = (a & 0xf0) >> 4;
+
+    switch (position)
+    {
+        case 0x00:
+            strcpy(buffer, "[POS BM");
+            break;
+        case 0x01:
+            strcpy(buffer, "[POS MM");
+            break;
+        case 0x02:
+            strcpy(buffer, "[POS TM");
+            break;
+        case 0x03:
+            strcpy(buffer, "[POS TL");
+            break;
+        case 0x04:
+            strcpy(buffer, "[POS TR");
+            break;
+        case 0x05:
+            strcpy(buffer, "[POS BL");
+            break;
+        case 0x06:
+            strcpy(buffer, "[POS BR");
+            break;
+    }
+
+    switch (style)
+    {
+        case 0x00:
+            strcat(buffer, " NV] ");
+            break;
+        case 0x04:
+            strcat(buffer, " SV] ");
+            break;
+        case 0x08:
+            strcat(buffer, " NI] ");
+            break;
+    }
+
+    return buffer;
+}
+
+char *is_symbol(byte a)
+{
+    switch (a)
+    {
+        case 0x7b:
+            return "↑";
+            break;
+        case 0x7c:
+            return "↓";
+            break; 
+        case 0x7d:
+            return "←";
+            break;
+        case 0x7e:
+            return "→";
+            break;
+        case 0x7f:
+            return "〜";
+            break;
+        case 0x80:
+            return "◯";
+            break;
+        case 0x81:
+            return "△";
+            break;
+        case 0x82:
+            return "×";
+            break;
+        case 0x83:
+            return "□";
+            break;
+        case 0x86:
+            return "↖";
+            break;
+        case 0x87:
+            return "↘";
+            break; 
+        case 0x88:
+            return "↗";
+            break;
+        case 0x89:
+            return "↙";
+            break;
+        case 0x93:
+            return "%";
+            break;
+        case 0x8a:
+            return "©";
+            break;
+        case 0x8d:
+            return "&";
+            break;
+        default:
+            return "";
+            break;
+    }
+}
+
+char *is_color(byte a)
+{
+    switch (a)
+    {
+        case 0x01:
+            return "PURPLE";
+            break;
+        case 0x02:
+            return "RED";
+            break;
+        case 0x03:
+            return "CYAN";
+            break;
+        case 0x04:
+            return "YELLOW";
+            break;
+        case 0x05:
+            return "PINK";
+            break;
+        case 0x06:
+            return "GREEN";
+            break;
+        case 0x07:
+            return "BLACK";
+            break;
+        default:
+            return "";
+            break;
+    }    
+}
+
+char *is_effect(byte a)
+{
+    switch (a)
+    {
+        case 0x00:
+            return "SHK_S";
+            break;
+        case 0x01:
+            return "SHK_L";
+            break;
+        case 0x02:
+            return "SHK_P";
+            break;
+        case 0x03:
+            return "BIG0_S";
+            break;
+        case 0x04:
+            return "BIG1_S";
+            break;
+        case 0x05:
+            return "BIG2_S";
+            break;
+        case 0x06:
+            return "BIG0_L";
+            break;
+        case 0x07:
+            return "BIG1_L";
+            break;
+        case 0x08:
+            return "BIG2_L";
+            break;
+        case 0x09:
+            return "BIG0_P";
+            break;
+        case 0x0a:
+            return "BIG1_P";
+            break;
+        case 0x0b:
+            return "BIG2_P";
+            break;
+        case 0x0c:
+            return "SML0_S";
+            break;
+        case 0x0d:
+            return "SML1_S";
+            break;
+        case 0x0e:
+            return "SML2_S";
+            break;
+        case 0x0f:
+            return "SML0_L";
+            break;
+        case 0x10:
+            return "SML1_L";
+            break;
+        case 0x11:
+            return "SML2_L";
+            break;
+        case 0x12:
+            return "SML0_P";
+            break;
+        case 0x13:
+            return "SML1_P";
+            break;
+        case 0x14:
+            return "SML2_P";
+            break;
+        case 0x15:
+            return "WAV_L";
+            break;
+        case 0x16:
+            return "WAV_H";
+            break;
+        case 0x17:
+            return "JMP0";
+            break;
+        case 0x18:
+            return "JMP1";
+            break;
+        case 0x19:
+            return "JMP2";
+            break;
+        default:
+            return "";
+            break;
+    }    
+}
